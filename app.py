ba93ee27614efe09b17cd87b3a4640c1148bdf8b
Maith3 <maithreyi@example.com>
2026-05-16 00:02:08 +0530

    ML API dev

---
+++ app.py
@@ -0,0 +1,45 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pandas as pd
+import joblib
+
+#Loading saved model
+model = joblib.load('Fmodel.joblib')
+
+#Creating a reference to a fastapi object
+app = FastAPI()
+
+#Input Schema
+class PatientData(BaseModel):
+    BMI: float
+    Systolic_BP: float
+    Diabetes_Status: int
+    Estimated_LDL: float
+    Total_Cholesterol: float
+    HDL: float
+
+#Home route
+@app.get("/")
+def home():
+    return {"message":"CVD Risk Prediction API Running"}
+
+#Prediction route
+@app.post("/predict")
+def predict(data: PatientData):
+    #Converting input to dataframe
+    input_data = pd.DataFrame([{
+        "BMI": data.BMI,
+        "Total Cholesterol (mg/dL)": data.Total_Cholesterol,
+        "HDL (mg/dL)": data.HDL,
+        "Diabetes Status": data.Diabetes_Status,
+        "Systolic BP": data.Systolic_BP,
+        "Estimated LDL (mg/dL)": data.Estimated_LDL
+    }])
+    
+    #Predict risk
+    prediction = model.predict(input_data)[0]
+    
+    #Return response
+    return{"Predicted CVD Risk Score": round(float(prediction),2)}
+    
+    
